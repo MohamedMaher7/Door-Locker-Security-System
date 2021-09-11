215c6e65acff480f3b567c5e721e34735ac6e38a
MohamedMaher7 <50000354+MohamedMaher7@users.noreply.github.com>
2021-09-11 02:10:27 +0200

    Add files via upload

---
+++ MC2/Debug/MC2_main.d
@@ -0,0 +1,21 @@
+MC2_main.o MC2_main.o: ../MC2_main.c ../common_macros.h ../micro_config.h \
+  ../std_types.h ../external_eeprom.h ../i2c.h ../motor.h ../timer.h \
+  ../uart.h ../buzzer.h
+
+../common_macros.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../external_eeprom.h:
+
+../i2c.h:
+
+../motor.h:
+
+../timer.h:
+
+../uart.h:
+
+../buzzer.h:
---
+++ MC2/Debug/buzzer.d
@@ -0,0 +1,10 @@
+buzzer.o buzzer.o: ../buzzer.c ../buzzer.h ../micro_config.h \
+  ../std_types.h ../common_macros.h
+
+../buzzer.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../common_macros.h:
