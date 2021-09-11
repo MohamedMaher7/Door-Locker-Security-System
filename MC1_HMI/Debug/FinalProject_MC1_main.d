495f5e1fe9cf687e1cb8af512441dca80b67abf4
MohamedMaher7 <50000354+MohamedMaher7@users.noreply.github.com>
2021-09-11 02:09:08 +0200

    Add files via upload

---
+++ MC1_HMI/Debug/FinalProject_MC1_main.d
@@ -0,0 +1,17 @@
+FinalProject_MC1_main.o FinalProject_MC1_main.o:  \
+ ../FinalProject_MC1_main.c ../common_macros.h ../micro_config.h \
+  ../std_types.h ../keypad.h ../lcd.h ../timer.h ../uart.h
+
+../common_macros.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../keypad.h:
+
+../lcd.h:
+
+../timer.h:
+
+../uart.h:
